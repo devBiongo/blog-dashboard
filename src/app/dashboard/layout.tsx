0b6042528618fe9f8c35a19dd016a4643649dacb
devBiongo <biongo.dev@gmail.com>
2024-03-21 10:37:03 +0900

    first commit

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+/** @format */
+
+export default function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    const aaa = ' ';
+    return <div>{children}</div>;
+}
